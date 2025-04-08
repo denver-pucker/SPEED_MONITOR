44bbfddfc6c485c607dea83465807dd0d36507ec
denver-pucker <denver_pucker@yahoo.com>
2025-04-08 15:56:59 -0600

    Daily update

--- TRIAL_CODE/ULTRASONIC_SENSOR_HC-SR04_TEST/src/ULTRASONIC_SENSOR_HC-SR04_TEST.cpp
+++ TRIAL_CODE/ULTRASONIC_SENSOR_HC-SR04_TEST/src/ULTRASONIC_SENSOR_HC-SR04_TEST.cpp
@@ -38,11 +38,18 @@ float deltaTime, feetPerSecond;
 float speedFPS, speedMPH;
 int count = 0;
 bool motion;
+bool sensorOneSet;
+bool sensorTwoSet;
+unsigned int currentTime, lastTime;
+unsigned int sensorOneTime, sensorTwoTime;
 
 float duration1 = 0.0;
 float duration2 = 0.0;
 float distance1 = 0.0;
 float distance2 = 0.0;
+
+// Calibration values
+float CAL_ECHO = 0.15;
 
 TCPClient TheClient; 
  
@@ -82,7 +89,114 @@ void setup() {
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
-
+  currentTime = millis();
+  motion = digitalRead(PIR);
+// Serial.printf("Motion detected %i\n",motion);
+// if(motion) {
+  timeSensor1 = measureTime(TRIGGERPIN1,ECHOPIN1);
+  while(!sensorOneSet) {
+    timeSensor1 = measureTime(TRIGGERPIN1,ECHOPIN1);
+    Serial.printf("timeSensor1 = %0.2f\n",timeSensor1);
+    if( timeSensor1 > CAL_ECHO) {
+      sensorOneSet = 1;
+      sensorOneTime = currentTime;
+      // Wait for sensor 2 to get triggered // need to figure out a timeout in case bike is coming from the opposite direction
+      while(sensorOneSet) {                                             
+        timeSensor2 = measureTime(TRIGGERPIN2,ECHOPIN2);
+        if( timeSensor2 > CAL_ECHO) {
+          sensorTwoTime = currentTime;
+          Serial.printf("timeSensor2 = %0.2f\n",timeSensor2);
+          sensorTwoSet = 1;
+          deltaTime = (sensorTwoTime - sensorOneTime) / 1000.0;
+          Serial.printf("deltaTime = %0.2f\n",deltaTime);
+    
+          // if(deltaTime > 0) {÷
+            Serial.printf("Objects measured = %i\n", count);
+            speedFPS = DISTANCE / deltaTime;    // speed = distance / time
+            Serial.printf("speedFPS = %0.2f FPS\n",speedFPS);
+            speedMPH = speedFPS * 0.681818;     // 1ft/sec = 0.681818 miles/hour
+            count++;
+            Serial.printf("Speed = %0.2f MPH\n\n", speedMPH);
+        }
+        sensorOneSet = 0;
+      }
+      // }
+    }
+  }
+}
+  
+  // Function to connect and reconnect as necessary to the MQTT server.
+  // Should be called in the loop function and it will take care if connecting.
+  void MQTT_connect() {
+    int8_t ret;
+    
+    // Return if already connected.
+    if (mqtt.connected()) {
+      return;
+    }
+    
+    Serial.print("Connecting to MQTT... ");
+    
+    while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+      Serial.printf("Error Code %s\n",mqtt.connectErrorString(ret));
+      Serial.printf("Retrying MQTT connection in 5 seconds...\n");
+      mqtt.disconnect();
+      delay(5000);  // wait 5 seconds and try again
+    }
+    Serial.printf("MQTT Connected!\n");
+  }
+  
+  bool MQTT_ping() {
+    static unsigned int last;
+    bool pingStatus;
+    
+    if ((millis()-last)>120000) {
+      Serial.printf("Pinging MQTT \n");
+      pingStatus = mqtt.ping();
+      if(!pingStatus) {
+        Serial.printf("Disconnecting \n");
+        mqtt.disconnect();
+      }
+      last = millis();
+    }
+    return pingStatus;
+  }
+  
+  float measureTime(int trigPin, int echoPin) {
+    digitalWrite(trigPin,LOW);
+    delay(2);
+    digitalWrite(trigPin,HIGH);
+    delay(10);
+    digitalWrite(trigPin,LOW);
+    float pulse = pulseIn(echoPin,HIGH);
+    float pulseTime = pulse * 0.00089;
+    // return pulseIn(echoPin,HIGH)*0.0000001;   //converts to seconds
+    // return pulseIn(echoPin,HIGH);   //converts to seconds
+    return pulseTime;
+  }
+  
+  float distance(int trigPin, int echoPin) {
+    digitalWrite(trigPin,LOW);
+    delay(2);
+    digitalWrite(trigPin,HIGH);
+    delay(10);
+    digitalWrite(trigPin,LOW);
+    float pulse = pulseIn(echoPin,HIGH);
+    // Calculates the Distance in mm
+    // ((time)*(Speed of sound))/ toward and backward of object) * 10
+    
+    // float distanceCalc = ((time /2.9) / 2); // Actual calculation in mm
+    float distanceCalc = ((pulse / 74.0) / 2.0); // Actual calculation in inches
+    return distanceCalc; // return calculated value
+  }
+  
+  float speed(uint32_t time, float sensor1, float sensor2) {
+    float mph;
+    // Calculates the speed based on each sensors time
+    // ((time)*(Speed of sound))/ toward and backward of object) * 10
+    return mph;
+  }
+  
   // Use this to get distance
   // distance1 = distance(TRIGGERPIN1,ECHOPIN1); // Use function to calculate the distance
   // Serial.printf("Distance too far %0.2f waiting for sensor1 to get triggered......\n", distance1);
@@ -91,109 +205,3 @@ void loop() {
   // Serial.printf("Sensor 2 distance = %0.2f in\n",distance2); // Output to serial
   // if(distance1 < 25.0) {
     // Wait for sensor 1 to get triggered
-  motion = digitalRead(PIR);
-  Serial.printf("Motion detected %i\n",motion);
-  if(motion) {
-  timeSensor1 = measureTime(TRIGGERPIN1,ECHOPIN1);
-  while(timeSensor1 == 0) {
-    // timeSensor1 = (measureTime(TRIGGERPIN1,ECHOPIN1) * 0.0000001);
-    timeSensor1 = measureTime(TRIGGERPIN1,ECHOPIN1);
-  }
-  // Serial.printf("timeSensor1 = %0.2f\n",timeSensor1);
-
-  // Wait for sensor 2 to get triggered
-  timeSensor2 = measureTime(TRIGGERPIN2,ECHOPIN2);
-  while(timeSensor2 == 0) {
-    // timeSensor2 = (measureTime(TRIGGERPIN2,ECHOPIN2) * 0.0000001);
-    timeSensor2 = measureTime(TRIGGERPIN2,ECHOPIN2);
-  }
-  // Serial.printf("timeSensor2 = %0.2f\n",timeSensor2);
-  
-  deltaTime = timeSensor2 - timeSensor1;
-
-  if(deltaTime > 0) {
-    Serial.printf("Objects measured = %i\n", count);
-    speedFPS = DISTANCE / deltaTime;    // speed = distance / time
-    Serial.printf("speedFPS = %0.2f FPS\n",speedFPS);
-    speedMPH = speedFPS * 0.681818;     // 1ft/sec = 0.681818 miles/hour
-    count++;
-    Serial.printf("Speed = %0.2f MPH\n\n", speedMPH);
-  }
-}
-  delay(500);
-
-
-
-}
-
-// Function to connect and reconnect as necessary to the MQTT server.
-// Should be called in the loop function and it will take care if connecting.
-void MQTT_connect() {
-  int8_t ret;
- 
-  // Return if already connected.
-  if (mqtt.connected()) {
-    return;
-  }
- 
-  Serial.print("Connecting to MQTT... ");
- 
-  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-       Serial.printf("Error Code %s\n",mqtt.connectErrorString(ret));
-       Serial.printf("Retrying MQTT connection in 5 seconds...\n");
-       mqtt.disconnect();
-       delay(5000);  // wait 5 seconds and try again
-  }
-  Serial.printf("MQTT Connected!\n");
-}
-
-bool MQTT_ping() {
-  static unsigned int last;
-  bool pingStatus;
-
-  if ((millis()-last)>120000) {
-      Serial.printf("Pinging MQTT \n");
-      pingStatus = mqtt.ping();
-      if(!pingStatus) {
-        Serial.printf("Disconnecting \n");
-        mqtt.disconnect();
-      }
-      last = millis();
-  }
-  return pingStatus;
-}
-
-float measureTime(int trigPin, int echoPin) {
-  digitalWrite(trigPin,LOW);
-  delay(2);
-  digitalWrite(trigPin,HIGH);
-  delay(10);
-  digitalWrite(trigPin,LOW);
-  float pulse = pulseIn(echoPin,HIGH);
-  float pulseTime = pulse * 0.00089;
-  // return pulseIn(echoPin,HIGH)*0.0000001;   //converts to seconds
-  // return pulseIn(echoPin,HIGH);   //converts to seconds
-  return pulseTime;
-}
-
-float distance(int trigPin, int echoPin) {
-  digitalWrite(trigPin,LOW);
-  delay(2);
-  digitalWrite(trigPin,HIGH);
-  delay(10);
-  digitalWrite(trigPin,LOW);
-  float pulse = pulseIn(echoPin,HIGH);
-  // Calculates the Distance in mm
-  // ((time)*(Speed of sound))/ toward and backward of object) * 10
-
-  // float distanceCalc = ((time /2.9) / 2); // Actual calculation in mm
-  float distanceCalc = ((pulse / 74.0) / 2.0); // Actual calculation in inches
-  return distanceCalc; // return calculated value
-}
-
-float speed(uint32_t time, float sensor1, float sensor2) {
-  float mph;
-    // Calculates the speed based on each sensors time
-  // ((time)*(Speed of sound))/ toward and backward of object) * 10
-  return mph;
-}
